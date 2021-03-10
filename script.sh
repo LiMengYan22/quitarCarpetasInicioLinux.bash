dc331853633c628c734a62dd82fdd75cfa2f90e1
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-10 15:46:36 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+gsettings set org.gnome.nautilus.desktop trash-icon-visible false
+
+
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
